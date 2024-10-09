21beaf8c45dd92b486058af3969a334dded811f6
L21660318 <l21660318@matehuala.tecnm.mx>
2024-10-09 07:16:40 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducción a Cómputo en la Nube</title>
+</head>
+<body>
+    <h1>Introducción a Cómputo en la Nube</h1>
+</body>
+</html>
